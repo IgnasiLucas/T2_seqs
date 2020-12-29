0e1c8cdad2230e76dc39a8f77ef57bced338e096
José Ignacio Lucas Lledó <ignasilucas@gmail.com>
2020-12-29 19:55:35 +0100

    Calia especificar la branca a l'hora de crear la xapa.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
   <!-- badges: start -->
-  [![Launch Rstudio Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/IgnasiLucas/T2_seqs/master?urlpath=rstudio)
+  [![Launch Rstudio Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/IgnasiLucas/T2_seqs/soca?urlpath=lab)
   <!-- badges: end -->
 
 # Tema 2. Anàlisi de seqüències
--- binderize.R
+++ binderize.R
@@ -2,6 +2,6 @@ library(holepunch)
 write_compendium_description(package = "T2_seqs",
                              description = "Pràctiques del tema 2. Anàlisi de seqüències.")
 write_dockerfile(maintainer="J. Ignacio Lucas Lledó")
-generate_badge()
+generate_badge(branch = "soca", urlpath = "lab")
 
 # Following instructions from https://github.com/karthik/holepunch
