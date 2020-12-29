4b849d3be67d76a2c2287fd0b21d529e175521cf
José Ignacio Lucas Lledó <ignasilucas@gmail.com>
2020-12-29 19:35:23 +0100

    Intentant fer un compendi.

--- guio.R
+++ guio.R
@@ -1,2 +1,7 @@
-holepunch::write_runtime()
-sessionInfo()
+library(holepunch)
+write_compendium_description(package = "T2_seqs",
+                             description = "Pràctiques del tema 2. Anàlisi de seqüències.")
+write_dockerfile(maintainer="J. Ignacio Lucas Lledó")
+generate_badge()
+
+# Following instructions from https://github.com/karthik/holepunch
