62a4711fbb238c03ab5d585fe71a2b2f20614b31
José Ignacio Lucas Lledó <ignasilucas@gmail.com>
2021-01-02 00:22:50 +0100

    Comence de nou sense pretensions de compendi.

--- binderize.R
+++
@@ -1,8 +0,0 @@
-library(holepunch)
-write_compendium_description(package = "T2_seqs",
-                             description = "Pràctiques del tema 2. Anàlisi de seqüències.")
-write_dockerfile(maintainer = "J. Ignacio Lucas Lledó",
-                 branch = "soca")
-generate_badge(branch = "soca", urlpath = "lab")
-
-# Following instructions from https://github.com/karthik/holepunch
